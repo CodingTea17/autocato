2031c1e712b44e10bacf724d5b6c3ce2a50d6142
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-07-09 10:54:26 -0400

    Removes pending development
    Not planning on doing that in the near future

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,5 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get 'welcome/index'
 
-  devise_for :users #, skip: :registrations
-  # devise_scope :user do
-  #   resource :registration,
-  #     only: [:edit, :update, :destroy, :create],
-  #     path: 'users',
-  #     controller: 'devise/registrations',
-  #     as: :user_registration do
-  #       get :cancel
-  #     end
-  # end
+  devise_for :users
 end
