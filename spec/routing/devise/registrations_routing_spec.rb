10203a3dae48279604ff2528b043493efbbc8547
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-07-09 09:38:34 -0400

    Removes a couple pending specs
    Not planning on implementing those things in the near future

--- spec/routing/devise/registrations_routing_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'Devise registrations routing', type: :routing do
-  xit { expect(get: '/users/sign_up').to_not be_routable }
-end
--- spec/routing/devise/sessions_routing_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'Devise sessions routing', type: :routing do
-  xit { expect(get: '/users/sign_in').to_not be_routable }
-end
