edbd0459301af138fc33763b7174bef7f3d38b37
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-07-11 21:32:43 -0400

    Adds helper methods for controller specs related to Devise

--- spec/support/configurations/devise.rb
+++ spec/support/configurations/devise.rb
@@ -1,3 +1,19 @@
+module DeviseControllerMacros
+  def attributes_with_fks_for(*args)
+    FactoryGirl.build_stubbed(*args).attributes.delete_if do |k, _v|
+      %w[id type created_at updated_at].member?(k)
+    end
+  end
+
+  def log_in(user, *other_args)
+    sign_in user, *other_args
+    @current_user = user
+  end
+
+  attr_reader :current_user
+end
+
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include DeviseControllerMacros, type: :controller
 end
