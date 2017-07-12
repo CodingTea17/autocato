15ef6558a267eb2a84ef552713efedfdb60a72d4
Amanda Dolan <amandacrawfordalc@gmail.com>
2017-07-12 09:24:49 -0400

    Adds more info to PageObjects::NotFound error message

--- spec/page_objects/base.rb
+++ spec/page_objects/base.rb
@@ -16,7 +16,7 @@ module PageObjects
       PageObjects.module_eval do
         define_method po_name do
           page_object = send(int_method_nm)
-          raise NotFound, po_name unless page_object.on_page?
+          raise NotFound.new(page, po_name) unless page_object.on_page?
           return page_object
         end
       end
@@ -46,13 +46,30 @@ module PageObjects
   end
 
   class NotFound < StandardError
-    def initialize(po_name = nil)
+    def initialize(page, po_name = nil)
+      @page = page
       details = " (#{po_name})" if po_name.present?
       msg = <<~TEXT
         The page object#{details} wasn't on the page when the object was
         accessed.
+          - The current page title is "#{title}".
+          - Classes applied to the main element are "#{main_classes}".
       TEXT
       super(msg)
+    end
+
+    def title
+      @page.title
+    end
+
+    def main_classes
+      main_element = @page.find('main')
+      if main_element.present?
+        classes = main_element[:class]
+        classes.present? ? classes : '(no classes applied to main element)'
+      else
+        '(no main element found)'
+      end
     end
   end
 end
